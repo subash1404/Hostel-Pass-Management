7a796efb097ffbafc127a8c376eff607ffcf36cf
subash1404 <subashvelusamy10@gmail.com>
2024-02-16 19:04:14 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hostel_pass_management/pages/login.dart';
+import 'package:hostel_pass_management/pages/newpass.dart';
+import 'package:hostel_pass_management/pages/profile.dart';
+import 'package:hostel_pass_management/pages/splash.dart';
 
-void main() {
-  runApp(const MyApp());
+final theme = ThemeData(
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: const Color.fromARGB(255, 156, 27, 255),
+  ),
+  textTheme: GoogleFonts.dmSansTextTheme(),
+);
+
+void main() async {
+  runApp(const App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const Scaffold(),
+      theme: theme,
+      home: ProfilePage(),
     );
   }
 }
